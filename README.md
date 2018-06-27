03e567cb13eef664fd5d4d41965ff1c6cd17ec7d
aloeguvner <aloeguvner@gmail.com>
2018-06-26 20:37:02 -0400

    ALOE_ConditionalChoices.js v1.0.0: Initial release, add tags to choices to conditionally hide or disable.

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
 
-## How to download
+## Instructions
+
+### How to download
 
 On the page of each plugin, locate the button labeled "Raw" in the upper-right. Right-click this button and choose "Save File As" to download.
 
-## Aloe Guvner Plugins
+### Repository Organization
+
+Two folder are included in this repository: "plugins" and "source_code".\
+The **source_code** folder are the plugins that I directly worked on, which use ES6+ features and are not compatible with RPG Maker MV versions prior to 1.6.0. If you would like to study how I implemented any features, this would be the version to look at.\
+The **plugins** folder is for the versions to install into your game, which do not use ES6+ features, meaning that these are compatible with RPG Maker MV 1.5.0+ and all browsers. These versions are transpiled using Babel.js.\
+
+### Contributions
+
+If you would like to contribute to any plugins, feel free to send a Pull Request!/
+
+### Bugs/Issues
+
+For any issues or feature requests for these plugins, I encourage you to open an Issue on the repository. Please ensure to include detailed information containing:
+* Which plugin has the issue
+* Steps to recreate the issue
+* Screenshots
+* Log of the console (press F8 during playtest)
+
+## Aloe Guvner Original Plugins
 
 ### Terms of Use
 
@@ -30,42 +50,42 @@ The following YEP extensions are released under the [MIT License](https://openso
 
 ### YEP Extension Plugin Links
 
-Title: Menu Parameter Control
-Extends: Status Core, Skill Core, In-Battle Status Window, Class Change Core, Item Core, Shop Menu Core
-Description:
-Control which parameters show in any menu!
-Perfect for games that don't use the traditional parameters (ex. games without magic, so no Magic Atk or Magic Def). Additionally, control what order the parameters appear.
-Install Instructions: Place below all YEP plugins.
+**Title**: Menu Parameter Control\
+**Extends**: Status Core, Skill Core, In-Battle Status Window, Class Change Core, Item Core, Shop Menu Core\
+**Description**:\
+Control which parameters show in any menu!\
+Perfect for games that don't use the traditional parameters (ex. games without magic, so no Magic Atk or Magic Def). Additionally, control what order the parameters appear.\
+**Install Instructions**: Place below all YEP plugins.\
 [YEP Menu Parameter Control](https://github.com/aloeguvner/rpg-maker-mv/blob/master/plugins/ALOE_YEP_MenuParameterControl.js)
 
-Title: Plugin Order Checker
-Extends: N/A
-Description: 
-Errors can occur if Yanfly plugins are added in the wrong order. The only way to check the order is manually against their website... Until now!
-This plugin will check the order of your Yanfly plugins and tell you if anything is wrong.
-Install Instructions: Install at the bottom of the Plugin Manager.
+**Title**: Plugin Order Checker\
+**Extends**: N/A\
+**Description**:\
+Errors can occur if Yanfly plugins are added in the wrong order. The only way to check the order is manually against their website... Until now!\
+This plugin will check the order of your Yanfly plugins and tell you if anything is wrong.\
+**Install Instructions**: Install at the bottom of the Plugin Manager.\
 [YEP Plugin Order Checker](https://github.com/aloeguvner/rpg-maker-mv/blob/master/plugins/ALOE_YEP_PluginOrderChecker.js)
 
-Title: Map Select Equip - Help Window
-Extends: YEP_MapSelectEquip
-Description:
+**Title**: Map Select Equip - Help Window\
+**Extends**: YEP_MapSelectEquip\
+**Description**:\
 The Map Select Equip plugin allows the player to select an equipment, and the ID# is saved to a variable.
-This extension creates a help window that displays the description of the equipment as the players are selecting it.
-Install Instructions: Install below YEP_MapSelectEquip
+This extension creates a help window that displays the description of the equipment as the players are selecting it.\
+**Install Instructions**: Install below YEP_MapSelectEquip\
 [YEP Map Select Equip - Help Window](https://github.com/aloeguvner/rpg-maker-mv/blob/master/plugins/ALOE_YEP_X_MapSelectEquip_Help.js)
 
-Title: Map Select Skill - Help Window
-Extends: YEP_MapSelectSkill
-Description:
-The Map Select Skill plugin allows the player to select a skill, and the ID# is saved to a variable.
-This extension creates a help window that displays the description of the skill as the players are selecting it.
-Install Instructions: Install below YEP_MapSelectSkill
+**Title**: Map Select Skill - Help Window\
+**Extends**: YEP_MapSelectSkill\
+**Description**:\
+The Map Select Skill plugin allows the player to select a skill, and the ID# is saved to a variable.\
+This extension creates a help window that displays the description of the skill as the players are selecting it.\
+**Install Instructions**: Install below YEP_MapSelectSkill\
 [YEP Map Select Skill - Help Window](https://github.com/aloeguvner/rpg-maker-mv/blob/master/plugins/ALOE_YEP_X_MapSelectSkill_Help.js)
 
-Title: Row Formation Actor Face
-Extends: YEP_RowFormation
-Description:
-The RowFormation plugin comes with a screen that allows the player to arrange their party's formation in rows.
-It uses the actor battle sprites by default, which can have strange effects if non-standard sprites are used. This extension allows the developer to choose to use the actor's face instead.
-Install Instructions: Install below YEP_RowFormation
+**Title**: Row Formation Actor Face\
+**Extends**: YEP_RowFormation\
+**Description**:\
+The RowFormation plugin comes with a screen that allows the player to arrange their party's formation in rows.\
+It uses the actor battle sprites by default, which can have strange effects if non-standard sprites are used. This extension allows the developer to choose to use the actor's face instead.\
+Install Instructions: Install below YEP_RowFormation\
 [YEP Row Formation - Actor Face](https://github.com/aloeguvner/rpg-maker-mv/blob/master/plugins/ALOE_YEP_RowFormation_ActorFace.js)
---
+++ source_code/ALOE_ConditionalChoices.js
@@ -0,0 +1,313 @@
+//=============================================================================
+// RPG Maker MV - Conditional Show Choices
+// ALOE_Event_Ladder_Counter.js
+//=============================================================================
+
+
+
+//=============================================================================
+/*:
+* @plugindesc Conditional Show Choices v1.0.0
+* Define conditions to hide or disable choices in the event menu
+* @author Aloe Guvner
+*
+* @param patternStart
+* @text Start Pattern
+* @type text
+* @desc Pattern that indicates the start of the conditions.
+* Recommended not to change this unless necessary.
+* @default <<
+*
+* @param patternEnd
+* @text End Pattern
+* @type text
+* @desc Pattern that indicates the end of the conditions.
+* Recommended not to change this unless necessary.
+* @default >>
+* 
+* @help
+* 
+* //=========================================================================
+* // Background
+* //=========================================================================
+*
+* This plugin allows developers to set conditions directly in the "Show Choices"
+* event command.
+* These conditions can determine whether the choice is hidden or disabled.
+*
+* No scripting knowledge or plugin commands are required!
+* Everything is done directly in the "Show Choices" event command, providing
+* an intuitive and efficient way to increase the depth of your story-telling
+* through more interesting choices.
+* 
+* //=========================================================================
+* // How to use
+* //=========================================================================
+*
+* The conditions to hide or disable the choice are written directly into the choice.
+*
+* An example is shown below. In this example, the first choice is hidden if
+* switch #1 is ON, and the second choice is hidden if variable #1 is greater
+* than zero. The third choice does not contain any conditions.
+*
+* ◆Show Choices：First Choice <<s[1]>>, Second Choice <<v[1] > 0>>, Third Choice (Window, Right, #3, #2)
+* ：When First Choice <<s[1]>> 
+*   ◆
+* ：When Second Choice <<v[1] > 0>> 
+*   ◆
+* ：When Third Choice 
+*   ◆
+* ：End
+*
+* The format of the conditions is:
+* <<HIDE,DISABLE>>
+*
+* If the HIDE condition is true, the choice will be hidden.
+* If the DISABLE condition is true, the choice will be disabled.
+*
+* To include only a HIDE condition, no comma is needed:
+* <<HIDE>>
+*
+* To include only a DISABLE condition, a comma is needed:
+* <<,DISABLE>>
+*
+* To include both a HIDE and a DISABLE condition, separate with a comma:
+* <<HIDE,DISABLE>>
+*
+* //=========================================================================
+* // Short-Hand Abbreviations
+* //=========================================================================
+*
+* The following short-hand abbreviations may be used inside the conditions:
+* s[x] --> Game Switch #x
+* v[x] --> Game Variable #x
+* a[x] --> Actor #x (database id)
+* p[x] --> Party Member x (index, which starts at 0)
+* t[x] --> Troop Member x (index, which starts at 0)
+*
+* //=========================================================================
+* // Javascript reference
+* //=========================================================================
+* 
+* All valid Javascript is usable within the conditions. Some useful references:
+* &&  --> AND
+* ||  --> OR
+* !   --> NOT
+*
+* === --> EQUAL TO
+* !== --> NOT EQUAL TO
+* >   --> GREATER THAN
+* >=  --> GREATER THAN OR EQUAL TO
+* <   --> LESS THAN
+* <=  --> LESS THAN OR EQUAL 
+*
+* +   --> ADDITION
+* -   --> SUBTRACTION / NEGATIVE
+* *   --> MULTIPLICATION
+* /   --> DIVISION
+* %   --> MODULO
+* **  --> EXPONENT
+*
+* //=========================================================================
+* // Examples
+* //=========================================================================
+* 
+* To hide a choice if Switch # 4 is ON:
+* My Choice <<s[4]>>
+*
+* To disable a choice if Switch #4 is ON:
+* My Choice <<,s[4]>>
+*
+* To hide a choice if Switch # 6 is OFF:
+* My Choice <<!s[6]>>
+*
+* To hide a choice if Variable # 2 is greater than 10:
+* My Choice <<v[2] > 10>>
+*
+* To hide a choice if Variable # 3 is less than or equal to 10:
+* My Choice <<v[3] <= 10>>
+*
+* To hide a choice if Variable #1 is less than 10 and disable if Variable #1
+* is less than 20:
+* My Choice <<v[1] < 10, v[1] < 20>>
+* 
+* To hide a choice if Switch #17 AND Switch #18 are ON:
+* My Choice <<s[17] && s[18]>>
+*
+* To disable a choice if Variable #17 equals 100 OR Variable #18 equals 100:
+* My Choice <<,v[17] === 100 || v[18] === 100>>
+*
+* To hide a choice if the Party leader's HP is less than 50:
+* My Choice <<p[0].hp < 50>>
+*
+* To disable a choice if the Actor #1's name is NOT Banjo:
+* My Choice <<,a[1].name() !== 'Banjo'>>
+*
+* To hide a choice if none in the party are named Kazooie:
+* My Choice <<p.every(member => member.name() !== 'Kazooie')>>
+*
+* To hide a choice if any in the party learned skill #4:
+* My Choice <<p.some(member => member.hasSkill(4)>>
+*
+* To disable a choice if every troop member's HP is below 25%:
+* My Choice <<,t.every(member => member.hp < member.mhp * .25)>>
+* 
+* =========================================================================
+* End of Help Section
+* =========================================================================
+*/
+
+(function () {
+
+    //=============================================================================
+    // Read Parameters
+    //=============================================================================
+
+    const params = PluginManager.parameters("ALOE_ConditionalChoices");
+
+
+    //=============================================================================
+    // New Methods - RPG Maker base engine classes
+    //=============================================================================
+    // Game_Message:
+    // --Create removeHiddenChoices to filter the choice array for only visible choices
+    // --Create updateVisibleIndexes to track original index for visible choices
+    // Window_ChoiceList:
+    // --Create markDisabledCommands to update window commands to disable choices
+    // --Create removeRegexPattern to remove the <<.*>> pattern from the choice
+    //=============================================================================
+
+    Game_Message.prototype.removeHiddenChoices = function (choices) {
+        const regex = new RegExp(`${params.patternStart}(.*)${params.patternEnd}`);
+        const s = $gameSwitches._data;
+        const v = $gameVariables._data;
+        const a = $gameActors._data;
+        const p = $gameParty.members();
+        const t = $gameTroop.members();
+        return choices
+            .filter(choice => {
+                const match = regex.exec(choice);
+                if (match && match[1]) {
+                    match[1] = match[1].split(",");
+                    if (match[1][0]) {
+                        try {
+                            const visible = !eval(match[1][0]);
+                            if (visible) {
+                                this._visibleChoices.push(choice);
+                            }
+                            return visible;
+                        } catch (error) {
+                            console.log(`Evaluation error for hide condition of choice: ${choice}`);
+                            console.log(`Evaluation formula: ${match[1][0]}`);
+                            console.error(error);
+                            this._visibleChoices.push(choice);
+                            return true;
+                        }
+                    }
+                }
+                this._visibleChoices.push(choice);
+                return true;
+            });
+    };
+
+    Game_Message.prototype.updateVisibleIndexes = function (choices) {
+        this._visibleChoices.forEach((visibleChoice, index) => {
+            this._visibleChoiceIndexes[index] = choices.indexOf(visibleChoice);
+        });
+    };
+
+    Window_ChoiceList.prototype.markDisabledCommands = function () {
+        const regex = new RegExp(`${params.patternStart}.*,(.*)${params.patternEnd}`);
+        const s = $gameSwitches._data;
+        const v = $gameVariables._data;
+        const a = $gameActors._data;
+        const p = $gameParty.members();
+        const t = $gameTroop.members();
+        this._list = this._list.map(listItem => {
+            let disabled;
+            const match = regex.exec(listItem.name);
+            if (match && match[1]) {
+                try {
+                    disabled = eval(match[1]);
+                } catch (error) {
+                    console.log(`Evaluation error for disable condition of choice: ${listItem.name}`);
+                    console.log(`Evaluation formula: ${match[1]}`);
+                    console.error(error);
+                    disabled = false;
+                }
+            }
+            listItem.enabled = !disabled;
+            return listItem;
+        });
+    };
+
+    Window_ChoiceList.prototype.removeRegexPattern = function () {
+        const regex = new RegExp(`${params.patternStart}(.*)${params.patternEnd}`);
+        this._list = this._list.map(listItem => {
+            listItem.name = listItem.name.replace(regex, "");
+            return listItem;
+        });
+    };
+
+    //=============================================================================
+    // Modifications - RPG Maker base engine methods (aliased)
+    //=============================================================================
+    // Game_Interpreter:
+    // --Modify the $gameMessage.choices() to remove the choices that are hidden
+    // Window_ChoiceList:
+    // --Modify makeCommandList to mark disabled choices and remove the regex pattern
+    // Game_Message:
+    // --Modify clear to initialize the visible choices mapping objects
+    //=============================================================================
+
+
+    const Game_Interpreter_setupChoices = Game_Interpreter.prototype.setupChoices
+    Game_Interpreter.prototype.setupChoices = function (params) {
+        Game_Interpreter_setupChoices.apply(this, arguments);
+        const choices = $gameMessage.removeHiddenChoices(params[0]);
+        $gameMessage.updateVisibleIndexes(params[0]);
+        const cancelType = params[1];
+        const defaultType = params.length > 2 ? params[2] : 0;
+        $gameMessage.setChoices(choices, defaultType, cancelType);
+    };
+
+    const Window_ChoiceList_makeCommandList = Window_ChoiceList.prototype.makeCommandList;
+    Window_ChoiceList.prototype.makeCommandList = function () {
+        Window_ChoiceList_makeCommandList.call(this);
+        this.markDisabledCommands();
+        this.removeRegexPattern();
+    };
+
+    const Game_Message_clear = Game_Message.prototype.clear;
+    Game_Message.prototype.clear = function () {
+        Game_Message_clear.call(this);
+        this._visibleChoices = [];
+        this._visibleChoiceIndexes = {};
+    };
+
+    //=============================================================================
+    // Overwrites - RPG Maker base engine methods
+    //=============================================================================
+    // Window_ChoiceList:
+    // --Overwrite drawItem to draw disabled commands in a lighter color.
+    // --Overwrite callOkHandler to utilize 
+    //=============================================================================
+
+    Window_ChoiceList.prototype.drawItem = function (index) {
+        const rect = this.itemRectForText(index);
+        this.resetTextColor();
+        this.changePaintOpacity(this.isCommandEnabled(index));
+        this.drawTextEx(this.commandName(index), rect.x, rect.y);
+    };
+
+    Window_ChoiceList.prototype.callOkHandler = function () {
+        $gameMessage.onChoice($gameMessage._visibleChoiceIndexes[this.index()]);
+        this._messageWindow.terminateMessage();
+        this.close();
+    };
+
+    //=============================================================================
+    // End of Plugin
+    //=============================================================================
+
+})();

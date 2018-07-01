13acca9b6f638bb0bc28bf6c9d40840c46a68297
aloeguvner <aloeguvner@gmail.com>
2018-07-01 18:25:27 -0400

    Add Aftermath Level Up - Sound Effect extension.

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ For any issues or feature requests for these plugins, I encourage you to open an
 
 ### Terms of Use
 
-The following plugins are free for use in both commercial and non-commercial games.
+The following original plugins are free for use in both commercial and non-commercial games.
 Credit is required either in the game, or in a text file which is included along with the game.
 Credit: Aloe Guvner
 
@@ -40,6 +40,8 @@ This plugin focuses on improving the user interface for mobile games created in 
 This plugin allows maximum flexibility for the developer, you can add as many or as few buttons as you want to the screen, and you can add these buttons on whichever screens you would like (i.e. map, menu, credits, title, etc.).\
 **Install Instructions**: No specific instructions. As with any plugin, if incompatibilities are found, try rearranging it.\
 [Mobile User Interface](https://github.com/aloeguvner/rpg-maker-mv/blob/master/plugins/ALOE_MobileUI.js)
+
+___
 
 **Title**: Player Notepad\
 **Description**:\
@@ -54,6 +56,7 @@ This opens up uses in:
 **Install Instructions**: No specific instructions. As with any plugin, if incompatibilities are found, try rearranging it.\
 [Player Notepad (In-Game Text Editor)](https://github.com/aloeguvner/rpg-maker-mv/blob/master/plugins/ALOE_PlayerNotepad.js)
 
+___
 
 **Title**: Conditional "Show Choices"\
 **Description**:\
@@ -65,6 +68,8 @@ Everything is done directly in the "Show Choices" event command, providing an in
 **Install Instructions**: If using the HIME Large Choices plugin, install this plugin *above* the HIME plugin. Besides that, no specific instructions. As with any plugin, if incompatibilities are found, try rearranging it.\
 [Conditional "Show Choices"](https://github.com/aloeguvner/rpg-maker-mv/blob/master/plugins/ALOE_ConditionalChoices.js)
 
+___
+
 **Title**: Syncronize Variables with Items\
 **Description**:\
 Some menu plugins require data to be stored as variables, which also has to be represented as item counts, or vice-versa.\
@@ -75,6 +80,8 @@ This plugin will synchronize the value of a variable with the inventory count of
 **Install Instructions**: No specific instructions. As with any plugin, if incompatibilities are found, try rearranging it.\
 [Syncronize Variables with Items](https://github.com/aloeguvner/rpg-maker-mv/blob/master/plugins/ALOE_Sync_Variables_Items.js)
 
+___
+
 **Title**: Syncronize Variables with Items\
 **Description**:\
 By default, the items/weapons/armors/skills are sorted by database ID number in menu windows, and there is no way to change this order from the editor.
@@ -84,6 +91,7 @@ This simple plugin allows the developer to use a notetag in the database in orde
 **Install Instructions**: No specific instructions. As with any plugin, if incompatibilities are found, try rearranging it.\
 [Item and Skill Sort Priority](https://github.com/aloeguvner/rpg-maker-mv/blob/master/plugins/ALOE_ItemSkillSortPriority.js)
 
+___
 
 **Title**: Syncronize Variables with Items\
 **Description**:\
@@ -115,6 +123,8 @@ Perfect for games that don't use the traditional parameters (ex. games without m
 **Install Instructions**: Place below all YEP plugins.\
 [YEP Menu Parameter Control](https://github.com/aloeguvner/rpg-maker-mv/blob/master/plugins/ALOE_YEP_MenuParameterControl.js)
 
+___
+
 **Title**: Plugin Order Checker\
 **Extends**: N/A\
 **Description**:\
@@ -122,6 +132,8 @@ Errors can occur if Yanfly plugins are added in the wrong order. The only way to
 This plugin will check the order of your Yanfly plugins and tell you if anything is wrong.\
 **Install Instructions**: Install at the bottom of the Plugin Manager.\
 [YEP Plugin Order Checker](https://github.com/aloeguvner/rpg-maker-mv/blob/master/plugins/ALOE_YEP_PluginOrderChecker.js)
+
+___
 
 **Title**: Map Select Equip - Help Window\
 **Extends**: YEP_MapSelectEquip\
@@ -131,6 +143,8 @@ This extension creates a help window that displays the description of the equipm
 **Install Instructions**: Install below YEP_MapSelectEquip\
 [YEP Map Select Equip - Help Window](https://github.com/aloeguvner/rpg-maker-mv/blob/master/plugins/ALOE_YEP_X_MapSelectEquip_Help.js)
 
+___
+
 **Title**: Map Select Skill - Help Window\
 **Extends**: YEP_MapSelectSkill\
 **Description**:\
@@ -139,6 +153,8 @@ This extension creates a help window that displays the description of the skill 
 **Install Instructions**: Install below YEP_MapSelectSkill\
 [YEP Map Select Skill - Help Window](https://github.com/aloeguvner/rpg-maker-mv/blob/master/plugins/ALOE_YEP_X_MapSelectSkill_Help.js)
 
+___
+
 **Title**: Row Formation Actor Face\
 **Extends**: YEP_RowFormation\
 **Description**:\
@@ -146,3 +162,12 @@ The RowFormation plugin comes with a screen that allows the player to arrange th
 It uses the actor battle sprites by default, which can have strange effects if non-standard sprites are used. This extension allows the developer to choose to use the actor's face instead.\
 Install Instructions: Install below YEP_RowFormation\
 [YEP Row Formation - Actor Face](https://github.com/aloeguvner/rpg-maker-mv/blob/master/plugins/ALOE_YEP_RowFormation_ActorFace.js)
+___
+
+**Title**: Level Up Aftermath Sound Effect\
+**Extends**: YEP_X_AftermathLevelUp\
+**Description**:\
+The Aftermath Level Up plugin shows a window describing the changes to an actor when they leveled up.\
+This extension allows the developer to play a Sound Effect to celebrate the actor leveling up. Additionally, each actor may use their own sound effect.\
+Install Instructions: Install below YEP_X_AftermathLevelUp\
+[YEP Aftermath Level Up - Sound Effect](https://github.com/aloeguvner/rpg-maker-mv/blob/master/plugins/ALOE_YEP_AftermathLevelUp_SE.js)
---
+++ plugins/ALOE_YEP_AftermathLevelUp_SE.js
@@ -0,0 +1,181 @@
+"use strict";
+
+/*:
+* @plugindesc v1.0.0 Plays sound effects in the aftermath
+* level up screen. 
+* @author Aloe Guvner
+*
+*
+* @param everyActor
+* @text Play for Every Actor
+* @type boolean
+* @default true
+* @desc true - Play SE for every actor that levels up
+* false - only play SE for the first actor
+*
+* @param soundEffectSetup
+* @text Sound Effect Setup
+* @type struct<soundEffectSetup>[]
+* @desc List the sound effect that will play for each
+* actor. Use 0 for the default.
+*
+*
+* 
+* @help
+*
+* //=============================================================================
+* Background:
+* //=============================================================================
+*
+* The Aftermath Level Up plugin shows a window describing the changes to an actor
+* when they leveled up.
+* This extension allows the developer to play a Sound Effect to celebrate the
+* actor leveling up. Additionally, each actor may use their own sound effect.
+*
+* If the actor doesn't have a sound effect specifically set for them, a default
+* is used. To set the default, set a sound effect in the parameters for the actor
+* ID of 0   (zero).
+*
+*
+* //=============================================================================
+* Terms of Use:
+* //=============================================================================
+* Free to use in Commercial or Non-Commercial projects.
+* 
+* Attribution:
+* This code is released under the MIT License.
+* https://opensource.org/licenses/MIT
+* 
+* Credits must be still given to Yanfly Engine Plugins as dictated by YEP.
+* 
+* //=============================================================================
+* Version History:
+* //=============================================================================
+* 
+* v1.0.0 (July 1 2018)
+* --Initial release
+* 
+* //=============================================================================
+* End of Help File
+* //=============================================================================
+*/
+
+/*~struct~soundEffect:
+ * @param name
+ * @text Sound Effect Name
+ * @type file
+ * @dir audio/se
+ * @desc Sound effect to play when the button is pressed.
+ * @default
+ * @require 1
+ * 
+ * @param volume
+ * @text Volume
+ * @type number
+ * @min 0
+ * @max 100
+ * @desc Volume of the sound effect, in %
+ * Allowed values: 0% - 100%
+ * @default 90
+ * 
+ * @param pitch
+ * @text Pitch
+ * @type number
+ * @min 50
+ * @max 150
+ * @desc Pitch of the sound effect, in %
+ * Allowed values: 50% - 150%
+ * @default 100
+ * 
+ * @param pan
+ * @text Pan
+ * @type number
+ * @min -100
+ * @max 100
+ * @desc Pan of the sound effect
+ * Allowed values: -100 - 100
+ * @default 0
+ * 
+*/
+
+/*~struct~soundEffectSetup:
+* @param actorId
+* @text Actor ID
+* @type number
+* @desc Actor ID that this sound effect plays for.
+* Use zero to create a default SE.
+*
+* @param soundEffect
+* @text Sound Effect
+* @type struct<soundEffect>
+* @desc Sound Effect to play when in the level up window.
+* 
+*/
+
+(function () {
+
+  if (Imported.YEP_X_AftermathLevelUp) {
+
+    //=============================================================================
+    // Utils
+    //=============================================================================
+    // Create a utility function to parse complex parameters.
+    //=============================================================================
+
+    Utils.recursiveParse = function (param) {
+      try {
+        return JSON.parse(param, function (key, value) {
+          try {
+            return this.recursiveParse(value);
+          } catch (e) {
+            return value;
+          }
+        }.bind(this));
+      } catch (e) {
+        return param;
+      }
+    };
+
+    //=============================================================================
+    // Parameters
+    //=============================================================================
+    // Read and parse parameters into a locally scoped Parameters object.
+    //=============================================================================
+
+    var Parameters = {};
+
+    Object.keys(PluginManager.parameters("ALOE_YEP_AftermathLevelUp_SE")).forEach(function (a) {
+      return Parameters[a] = Utils.recursiveParse(PluginManager.parameters("ALOE_YEP_AftermathLevelUp_SE")[a]);
+    });
+
+    //=============================================================================
+    // Scene_Battle
+    //=============================================================================
+    // Alias the aftermath setup to play a sound effect, if defined.
+    //=============================================================================
+
+    var Scene_Battle_setupNextAftermathLevelUpActor = Scene_Battle.prototype.setupNextAftermathLevelUpActor;
+    Scene_Battle.prototype.setupNextAftermathLevelUpActor = function () {
+      Scene_Battle_setupNextAftermathLevelUpActor.call(this);
+      var actorId = this._levelUpActor.actorId();
+      var se = Parameters.soundEffectSetup.find(function (param) {
+        return param.actorId === actorId;
+      }) || Parameters.soundEffectSetup.find(function (param) {
+        return param.actorId === 0;
+      });
+      if (!Parameters.everyActor && this._levelUpSePlayed) {
+        return;
+      }
+      if (se) {
+        AudioManager.playSe(se.soundEffect);
+        this._levelUpSePlayed = true;
+      }
+    };
+
+    //=============================================================================
+    // End of extension
+    //=============================================================================
+  } else {
+    console.log("ALOE_YEP_AftermathLevelUp_SE is enabled, but YEP_X_AftermathLevelUp is not enabled.");
+  }
+})();

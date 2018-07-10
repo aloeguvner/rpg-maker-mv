0c1684034e74ecf79c6fbc33781cb0bf8440ac8f
aloeguvner <aloeguvner@gmail.com>
2018-07-10 17:00:53 -0400

    AftermathLevelUpSE - Added polyfill for Array.prototype.find for older versions of MV.

--- plugins/ALOE_YEP_AftermathLevelUp_SE.js
+++ plugins/ALOE_YEP_AftermathLevelUp_SE.js
@@ -175,6 +175,58 @@
     //=============================================================================
     // End of extension
     //=============================================================================
+
+    //=============================================================================
+    // Polyfill for Array.prototype.find for MV versions 1.5 and older
+    //=============================================================================
+
+    // https://tc39.github.io/ecma262/#sec-array.prototype.find
+    if (!Array.prototype.find) {
+      Object.defineProperty(Array.prototype, 'find', {
+        value: function(predicate) {
+        // 1. Let O be ? ToObject(this value).
+          if (this == null) {
+            throw new TypeError('"this" is null or not defined');
+          }
+
+          var o = Object(this);
+
+          // 2. Let len be ? ToLength(? Get(O, "length")).
+          var len = o.length >>> 0;
+
+          // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+          if (typeof predicate !== 'function') {
+            throw new TypeError('predicate must be a function');
+          }
+
+          // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+          var thisArg = arguments[1];
+
+          // 5. Let k be 0.
+          var k = 0;
+
+          // 6. Repeat, while k < len
+          while (k < len) {
+            // a. Let Pk be ! ToString(k).
+            // b. Let kValue be ? Get(O, Pk).
+            // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+            // d. If testResult is true, return kValue.
+            var kValue = o[k];
+            if (predicate.call(thisArg, kValue, k, o)) {
+              return kValue;
+            }
+            // e. Increase k by 1.
+            k++;
+          }
+
+          // 7. Return undefined.
+          return undefined;
+        },
+        configurable: true,
+        writable: true
+      });
+    }
+
   } else {
     console.log("ALOE_YEP_AftermathLevelUp_SE is enabled, but YEP_X_AftermathLevelUp is not enabled.");
   }

a62ae44751e2d3b40581185259e6a0df43e00008
aloeguvner <aloeguvner@gmail.com>
2018-07-28 19:05:16 -0600

    v1.0.1 Add polyfill for Array.prototype.findIndex for older versions of MV.

--- plugins/ALOE_CommandIcons.js
+++ plugins/ALOE_CommandIcons.js
@@ -10,7 +10,7 @@ Imported.ALOE_CommandIcons = true;
 
 //=============================================================================
 /*:
- * @plugindesc v1.0.0 Allows the developer to add icons to any command
+ * @plugindesc v1.0.1 Allows the developer to add icons to any command
  * in a command window.
  * @author Aloe Guvner
  * 
@@ -161,7 +161,8 @@ Imported.ALOE_CommandIcons = true;
  * //=============================================================================
  * // Version History
  * //=============================================================================
- * 
+ * v1.0.1
+ * --Added polyfill for Array.prototype.findIndex for older versions of MV
  * v1.0.0
  * --Initial release
  * 
@@ -335,4 +336,55 @@ if (ALOE.CommandIconsParams.debugMode) {
             console.log(logObject);
         };
     })();
+}
+
+//=============================================================================
+// Polyfill - Array findIndex - For RPG Maker MV versions less than 1.6.0
+//=============================================================================
+
+// https://tc39.github.io/ecma262/#sec-array.prototype.findIndex
+if (!Array.prototype.findIndex) {
+    Object.defineProperty(Array.prototype, 'findIndex', {
+        value: function value(predicate) {
+            // 1. Let O be ? ToObject(this value).
+            if (this == null) {
+                throw new TypeError('"this" is null or not defined');
+            }
+
+            var o = Object(this);
+
+            // 2. Let len be ? ToLength(? Get(O, "length")).
+            var len = o.length >>> 0;
+
+            // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+            if (typeof predicate !== 'function') {
+                throw new TypeError('predicate must be a function');
+            }
+
+            // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+            var thisArg = arguments[1];
+
+            // 5. Let k be 0.
+            var k = 0;
+
+            // 6. Repeat, while k < len
+            while (k < len) {
+                // a. Let Pk be ! ToString(k).
+                // b. Let kValue be ? Get(O, Pk).
+                // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+                // d. If testResult is true, return k.
+                var kValue = o[k];
+                if (predicate.call(thisArg, kValue, k, o)) {
+                    return k;
+                }
+                // e. Increase k by 1.
+                k++;
+            }
+
+            // 7. Return -1.
+            return -1;
+        },
+        configurable: true,
+        writable: true
+    });
 }

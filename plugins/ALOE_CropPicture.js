135eac278aed2a70963aa7380077296ed9769cd3
aloeguvner <aloeguvner@gmail.com>
2019-01-19 13:55:21 -0600

    v1.1.0 Add polyfill and plugin command
    Add polyfill for Object.values for old MV versions
    Add plugin command to reset the cropping
    Add an alias to reset the crop when the picture is erased based
    on plugin parameter.

--- plugins/ALOE_CropPicture.js
+++ plugins/ALOE_CropPicture.js
@@ -21,8 +21,14 @@ if (typeof Imported !== 'undefined') {
 } //=============================================================================
 
 /*:
-* @plugindesc Crop a picture using a plugin command
+* @plugindesc Crop a picture using a plugin command v1.1.1
 * @author Aloe Guvner
+*
+* @param resetCropOnErase
+* @text Reset Crop on Erase
+* @type boolean
+* @desc Resets the crop settings when the picture is erased
+* @default true
 *
 * @help
 * 
@@ -34,18 +40,26 @@ if (typeof Imported !== 'undefined') {
 * event commands. This plugin provides a plugin command to crop a picture.
 * 
 * =========================================================================
-* Version History
+* Plugin Commands
 * =========================================================================
 *
 * CropPicture [pictureId] [x] [y] [width] [height]
 * ex. CropPicture 4 100 40 250 150
-* This would crop picture 4 starting at x=100, y=40 and crop leaving 250
+* This will crop picture 4 starting at x=100, y=40 and crop leaving 250
 * as the width and 150 as the height. The x and y positions are relative
 * to the origin of the picture.
+*
+* CropPicture reset [pictureId]
+* ex. CropPicture reset 4
+* This will reset the cropping on picture 4.
+*
 * =========================================================================
 * Version History
 * =========================================================================
 *
+* v1.1.0 - January 19 2019:
+* --Add plugin command to reset the cropping
+* --Add Object.values polyfill for old versions of MV
 * v1.0.0 - January 19 2019:
 * --Initial Release
 *
@@ -53,19 +67,42 @@ if (typeof Imported !== 'undefined') {
 * End of Help Section
 * =========================================================================
 */
+// =========================================================================
+// Create Namespace
+// =========================================================================
 
 
 if (typeof ALOE !== 'undefined') {
   ALOE.CropPicture = {
-    Alias: {}
+    Alias: {},
+    Parameters: {}
   };
 } else {
   window.ALOE = {
     CropPicture: {
-      Alias: {}
+      Alias: {},
+      Parameters: {}
     }
   };
 }
+
+(function () {
+  var pluginName = 'ALOE_CropPicture';
+  var params = PluginManager.parameters(pluginName);
+
+  if (Object.keys(params).length === 0) {
+    console.error("Error reading parameters, ensure that the plugin is named ".concat(pluginName, ".js\n        and it is up to date in the Plugin Manager"));
+  }
+
+  ALOE.CropPicture.Parameters.resetCropOnErase = params.resetCropOnErase === 'true';
+})(); // =========================================================================
+// Game_Picture
+// 
+// Create property of the Game_Picture and associated methods to hold crop
+// information.
+// Property is set from the plugin command and read from the sprite.
+// =========================================================================
+
 
 ALOE.CropPicture.Alias.initBasic = Game_Picture.prototype.initBasic;
 
@@ -77,12 +114,12 @@ Game_Picture.prototype.initBasic = function () {
     width: 0,
     height: 0
   };
+  this._resetCrop = false;
 };
 
 Game_Picture.prototype.getCrop = function () {
   return this._crop;
-}; // params is {x: int, y: int, width: int, height: int}
-
+};
 
 Game_Picture.prototype.setCrop = function (x, y, width, height) {
   this._crop = {
@@ -92,6 +129,19 @@ Game_Picture.prototype.setCrop = function (x, y, width, height) {
     height: height
   };
 };
+
+Game_Picture.prototype.resetCrop = function (bool) {
+  this._resetCrop = bool;
+};
+
+Game_Picture.prototype.needsResetCrop = function () {
+  return this._resetCrop;
+}; // =========================================================================
+// Sprite_Picture
+//
+// Read the crop data from the Game_Picture object and update the sprite frame.
+// =========================================================================
+
 
 ALOE.CropPicture.Alias.update = Sprite_Picture.prototype.update;
 
@@ -112,7 +162,32 @@ Sprite_Picture.prototype.updateCrop = function () {
       return picture.getCrop()[key] = 0;
     });
   }
+
+  if (picture.needsResetCrop()) {
+    this.setFrame(0, 0, this.bitmap.baseTexture.width, this.bitmap.baseTexture.height);
+    picture.resetCrop(false);
+  }
 };
+
+if (ALOE.CropPicture.Parameters.resetCropOnErase) {
+  // Reset the stage just before the bitmap is destroyed 
+  ALOE.CropPicture.Alias.updateBitmap = Sprite_Picture.prototype.updateBitmap;
+
+  Sprite_Picture.prototype.updateBitmap = function () {
+    var picture = this.picture();
+
+    if (!picture && this.bitmap && this.bitmap.baseTexture) {
+      this.setFrame(0, 0, this.bitmap.baseTexture.width, this.bitmap.baseTexture.height);
+    }
+
+    ALOE.CropPicture.Alias.updateBitmap.call(this);
+  };
+} // =========================================================================
+// Game_Interpreter
+//
+// Create the plugin command to set the crop property on the Game_Picture.
+// =========================================================================
+
 
 ALOE.CropPicture.Alias.pluginCommand = Game_Interpreter.prototype.pluginCommand;
 
@@ -120,13 +195,35 @@ Game_Interpreter.prototype.pluginCommand = function (command, args) {
   ALOE.CropPicture.Alias.pluginCommand.call(this, command, args);
 
   if (command.toLowerCase() === 'croppicture') {
-    var ints = args.map(function (arg) {
-      return parseInt(arg);
-    });
-    var picture = $gameScreen.picture(ints[0]);
+    if (args[0].toLowerCase() === 'reset') {
+      var picture = $gameScreen.picture(parseInt(args[1]));
 
-    if (picture) {
-      picture.setCrop(ints[1], ints[2], ints[3], ints[4]);
+      if (picture) {
+        picture.resetCrop(true);
+      }
+    } else {
+      var ints = args.map(function (arg) {
+        return parseInt(arg);
+      });
+
+      var _picture = $gameScreen.picture(ints[0]);
+
+      if (_picture) {
+        _picture.setCrop(ints[1], ints[2], ints[3], ints[4]);
+      }
     }
   }
-};
+}; // =========================================================================
+// Polyfills
+//
+// Simple polyfills to support older versions of MV.
+// =========================================================================
+
+
+if (!Object.values) {
+  Object.values = function (object) {
+    return Object.keys(object).map(function (key) {
+      return object[key];
+    });
+  };
+}
--- source_code/ALOE_CropPicture.js
+++ source_code/ALOE_CropPicture.js
@@ -11,8 +11,14 @@ if (typeof Imported !== 'undefined') {
 
 //=============================================================================
 /*:
-* @plugindesc Crop a picture using a plugin command
+* @plugindesc Crop a picture using a plugin command v1.1.1
 * @author Aloe Guvner
+*
+* @param resetCropOnErase
+* @text Reset Crop on Erase
+* @type boolean
+* @desc Resets the crop settings when the picture is erased
+* @default true
 *
 * @help
 * 
@@ -24,18 +30,26 @@ if (typeof Imported !== 'undefined') {
 * event commands. This plugin provides a plugin command to crop a picture.
 * 
 * =========================================================================
-* Version History
+* Plugin Commands
 * =========================================================================
 *
 * CropPicture [pictureId] [x] [y] [width] [height]
 * ex. CropPicture 4 100 40 250 150
-* This would crop picture 4 starting at x=100, y=40 and crop leaving 250
+* This will crop picture 4 starting at x=100, y=40 and crop leaving 250
 * as the width and 150 as the height. The x and y positions are relative
 * to the origin of the picture.
+*
+* CropPicture reset [pictureId]
+* ex. CropPicture reset 4
+* This will reset the cropping on picture 4.
+*
 * =========================================================================
 * Version History
 * =========================================================================
 *
+* v1.1.0 - January 19 2019:
+* --Add plugin command to reset the cropping
+* --Add Object.values polyfill for old versions of MV
 * v1.0.0 - January 19 2019:
 * --Initial Release
 *
@@ -44,26 +58,62 @@ if (typeof Imported !== 'undefined') {
 * =========================================================================
 */
 
+// =========================================================================
+// Create Namespace
+// =========================================================================
+
 if (typeof ALOE !== 'undefined') {
-    ALOE.CropPicture = {Alias: {}};
+    ALOE.CropPicture = {Alias: {}, Parameters: {}};
 } else {
-    window.ALOE = {CropPicture: {Alias: {}}};
+    window.ALOE = {CropPicture: {Alias: {}, Parameters: {}}};
 }
+
+(() => {
+    const pluginName = 'ALOE_CropPicture';
+    const params = PluginManager.parameters(pluginName);
+    if (Object.keys(params).length === 0) {
+        console.error(`Error reading parameters, ensure that the plugin is named ${pluginName}.js
+        and it is up to date in the Plugin Manager`);
+    }
+    ALOE.CropPicture.Parameters.resetCropOnErase = params.resetCropOnErase === 'true';
+})();
+
+// =========================================================================
+// Game_Picture
+// 
+// Create property of the Game_Picture and associated methods to hold crop
+// information.
+// Property is set from the plugin command and read from the sprite.
+// =========================================================================
 
 ALOE.CropPicture.Alias.initBasic = Game_Picture.prototype.initBasic;
 Game_Picture.prototype.initBasic = function() {
     ALOE.CropPicture.Alias.initBasic.apply(this, arguments);
     this._crop = {x: 0, y: 0, width: 0, height: 0};
+    this._resetCrop = false;
 };
 
 Game_Picture.prototype.getCrop = function() {
     return this._crop;
 };
 
-// params is {x: int, y: int, width: int, height: int}
 Game_Picture.prototype.setCrop = function(x, y, width, height) {
     this._crop = { x, y, width, height };
 };
+
+Game_Picture.prototype.resetCrop = function(bool) {
+    this._resetCrop = bool;
+};
+
+Game_Picture.prototype.needsResetCrop = function() {
+    return this._resetCrop;
+};
+
+// =========================================================================
+// Sprite_Picture
+//
+// Read the crop data from the Game_Picture object and update the sprite frame.
+// =========================================================================
 
 ALOE.CropPicture.Alias.update = Sprite_Picture.prototype.update;
 Sprite_Picture.prototype.update = function() {
@@ -79,14 +129,51 @@ Sprite_Picture.prototype.updateCrop = function() {
         this.setFrame(...Object.values(picture.getCrop()));
         Object.keys(picture.getCrop()).forEach(key => picture.getCrop()[key] = 0);
     }
+    if (picture.needsResetCrop()) {
+        this.setFrame(0, 0, this.bitmap.baseTexture.width, this.bitmap.baseTexture.height);
+        picture.resetCrop(false);
+    }
 };
+
+if (ALOE.CropPicture.Parameters.resetCropOnErase) {
+    // Reset the stage just before the bitmap is destroyed 
+    ALOE.CropPicture.Alias.updateBitmap = Sprite_Picture.prototype.updateBitmap;
+    Sprite_Picture.prototype.updateBitmap = function() {
+        var picture = this.picture();
+        if (!picture && this.bitmap && this.bitmap.baseTexture) { 
+            this.setFrame(0, 0, this.bitmap.baseTexture.width, this.bitmap.baseTexture.height); 
+        }
+        ALOE.CropPicture.Alias.updateBitmap.call(this);
+    };
+}
+
+// =========================================================================
+// Game_Interpreter
+//
+// Create the plugin command to set the crop property on the Game_Picture.
+// =========================================================================
 
 ALOE.CropPicture.Alias.pluginCommand = Game_Interpreter.prototype.pluginCommand;
 Game_Interpreter.prototype.pluginCommand = function(command, args) {
     ALOE.CropPicture.Alias.pluginCommand.call(this, command, args);
     if (command.toLowerCase() === 'croppicture') {
-        const ints = args.map(arg => parseInt(arg));
-        const picture = $gameScreen.picture(ints[0]);
-        if (picture) { picture.setCrop(ints[1], ints[2], ints[3], ints[4]); }
+        if (args[0].toLowerCase() === 'reset') {
+            const picture = $gameScreen.picture(parseInt(args[1]));
+            if (picture) { picture.resetCrop(true); }
+        } else {
+            const ints = args.map(arg => parseInt(arg));
+            const picture = $gameScreen.picture(ints[0]);
+            if (picture) { picture.setCrop(ints[1], ints[2], ints[3], ints[4]); }
+        }
     }
 };
+
+// =========================================================================
+// Polyfills
+//
+// Simple polyfills to support older versions of MV.
+// =========================================================================
+
+if (!Object.values) {
+    Object.values = object => Object.keys(object).map(key => object[key]);
+}
